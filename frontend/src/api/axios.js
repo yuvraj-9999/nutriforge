40fd350b240e44efca738e0b3332258e492cfbfd
Yuvraj Sabale <yuvrajsabale999@gmail.com>
2026-06-10 20:38:54 +0530

    fixed production issue base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use(
